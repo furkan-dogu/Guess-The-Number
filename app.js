744981d1746f71f4948f5956ed81a0e499cd5747
Furkan Doğu <furkandogu2018@gmail.com>
2023-11-11 00:40:52 +0300

    ses eklendi, hatalar düzeltildi

--- app.js
+++ app.js
@@ -9,39 +9,48 @@ const restart = document.querySelector(".btn-restart")
 const score = document.querySelector(".score")
 const scoreNum = document.querySelector(".score-number")
 const h1 = document.querySelector("h1")
-const container = document.querySelector(".container")
 
 let random = Math.floor(Math.random() * 101)
 
 check.addEventListener("click", () => {
     let tahmin = parseInt(input.value)
 
+    if(scoreNum.textContent > 1){
+        scoreNum.textContent--
+    } else if(scoreNum.textContent == 1) {
+        h1.textContent = "😢 You lost the game 😢"
+        loser.play()
+        tekrar()
+        document.querySelector(".text-div").style.display = "none"
+    } 
+
     if(tahmin < 0 || tahmin > 100 || isNaN(tahmin)) {
+        text.style.display = "block"
         text.textContent = "Enter a number between 0 and 100"
         text2.style.display = "none"
         scoreNum.textContent++
     } else {
         if(tahmin == random) {
-            h1.textContent =  `🎉 Congratulations. You got it in ${+(testNum.textContent) + 1} guesses 🎉`
+            h1.textContent =  `🎉 Congratulations 🎉`
+            text.textContent = `You got it in ${+(testNum.textContent) + 1} guesses`
+            applaud.play()
+            loser.pause()
             tekrar()
-            
+            text.style.display = "block"
+            document.querySelector(".text-div").style.display = "block"
         } else if(tahmin < random) {
             text.textContent = "🔼 Increase 🔼"
             firstNum.textContent = tahmin
             text2.style.display = "block"
             testNum.textContent++
+            text.style.display = "block"
         } else {
             text.textContent = "🔽 Reduce 🔽"
             secondNum.textContent = tahmin
             text2.style.display = "block" 
             testNum.textContent++
+            text.style.display = "block"
         }
-    }
-    if(scoreNum.textContent > 1){
-        scoreNum.textContent--
-    } else if(scoreNum.textContent == 1) {
-        h1.textContent = "😢 You lost the game 😢"
-        tekrar()
     }
 })
 
@@ -61,11 +70,10 @@ restart.addEventListener("click", () => {
 })
 
 const tekrar = () => {
-    text.textContent = ""
-    check.style.display = "none"
-    restart.style.display = "block"
-    text2.textContent = ""
-    score.style.display = "none"
-    input.style.display = "none"
-    container.style.justifyContent = "center"
+    check.style.display = "none";
+    restart.style.display = "block";
+    text2.textContent = "";
+    score.style.display = "none";
+    input.style.display = "none";
+    h1.style = "border: 5px solid turquoise; box-shadow: 0 0 20px turquoise , inset 0 0 20px turquoise; background: rgba(0, 0, 0, 0.7); color: white; border-radius: 20px; padding: 1rem; ";
 }
